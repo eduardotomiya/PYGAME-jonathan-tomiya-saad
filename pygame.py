97bc96331dae3312372393a8c9b3e6f02fb93db2
Jonathanflesch <jonathanflesch10@gmail.com>
2023-05-24 00:35:08 -0300

    teste

---
+++ pygame.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+width = 800
+height = 400
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Planet Pong')
+
+
+game = True
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    window.fill((255,255,255))
+    pygame.display.update()
+pygame.quit()
